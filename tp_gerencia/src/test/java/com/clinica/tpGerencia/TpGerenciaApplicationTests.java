5ba859c78571378689b9b5122af655224103805d
Arthur Alexi <arthuralexi@hotmail.com>
2024-05-01 07:23:43 -0300

    chore: initial configs

---
+++ tp_gerencia/src/test/java/com/clinica/tpGerencia/TpGerenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinica.tpGerencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpGerenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
